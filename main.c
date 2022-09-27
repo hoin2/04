13c7b78d2144167648be16cd0b823c79e0d940c3
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-09-27 16:16:48 +0900

    1

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[]) {
+	int x,y,z,m;
+	int a,b,c;
+	x=1;
+	y=3;
+	z=5;
+	a=3;
+	b=4;
+	c=5;
+	
+	y=a*x^+b*x+c;
+	m=(x+y+z)/3;
+	
+	printf("y=%d,m=%d",y,m);
+	
+	return 0;
+}
